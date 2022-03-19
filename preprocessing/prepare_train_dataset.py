5cb4e7bb6be5b3b2278398cabbb728f4be9f2e1a
Daniel Levenson <dleve123@gmail.com>
2022-03-19 13:20:40 -0400

    Try out a batch tokenizer

--- preprocessing/prepare_train_dataset.py
+++ preprocessing/prepare_train_dataset.py
@@ -10,14 +10,14 @@ def load_paper_summaries(loc = "data/paper/train.jsonl"):
         provided by the paper from local storage
     """
 
-    papers_with_negative_examples = []
+    docs_with_negative_examples = []
     with open(loc, "r") as f:
         for line in f:
             obj = json.loads(line)
             if len(obj["negative_examples"]) > 0:
-                papers_with_negative_examples.append(obj)
-            
-    return papers_with_negative_examples
+                docs_with_negative_examples.append(obj)
+
+    return docs_with_negative_examples
 
 def tokenize_data(tokenizer, data):
     """
@@ -30,10 +30,31 @@ def tokenize_data(tokenizer, data):
         for negative in doc["negative_examples"]:
             tokenized.append(tokenizer(
                 [positive, negative],
-                text_pair=[doc["source_text"]] * 2, 
-                return_tensors="pt", 
+                text_pair=[doc["source_text"]] * 2,
+                return_tensors="pt",
                 truncation='only_second',
                 padding=True
             ))
+
+    return tokenized
+
+def tokenize_data_batch(tokenizer, data):
+    """
+        Returns a tensor with pairs of summaries
+        [positive example, negative example]
+    """
+    tokenized = []
+    for doc in data:
+        positive_example = doc["positive_examples"]
+        negative_examples = doc["negative_examples"]
+        all_examples =  positive_example + negative_examples
+        tokenized.append(tokenizer(
+            all_examples,
+            text_pair=[doc["source_text"]] * len(all_examples),
+            return_tensors="pt",
+            truncation='only_second',
+            padding='max_length',
+            max_length=tokenizer.model_max_length,
+        ))
 
     return tokenized
